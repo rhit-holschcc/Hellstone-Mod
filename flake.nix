6be0df995734ace98954e514050407af3ec1329e
Carson Holscher <holschcc@rose-hulman.edu>
2025-05-05 11:22:21 -0400

    Added the forge mdk

--- flake.nix
+++ flake.nix
@@ -23,15 +23,40 @@
           jdk = prev."jdk${toString javaVersion}";
         in
         {
+          inherit jdk;
           boot = prev.boot.override { inherit jdk; };
           clojure = prev.clojure.override { inherit jdk; };
           leiningen = prev.leiningen.override { inherit jdk; };
+          maven = prev.maven.override { jdk_headless = jdk; };
+          gradle = prev.gradle.override { java = jdk; };
+          lombok = prev.lombok.override { inherit jdk; };
         };
 
       devShells = forEachSupportedSystem ({ pkgs }: {
         default = pkgs.mkShell {
-          packages = with pkgs; [ boot clojure leiningen ];
+          packages = with pkgs; [
+            boot
+            clojure
+            leiningen
+            gcc
+            gradle
+            jdk
+            maven
+            ncurses
+            patchelf
+            zlib
+            jdt-language-server
+            jetbrains.idea-community-bin
+          ];
         };
+        shellHook =
+          let
+            loadLombok = "-javaagent:${pkgs.lombok}/share/java/lombok.jar";
+            prev = "\${JAVA_TOOL_OPTIONS:+ $JAVA_TOOL_OPTIONS}";
+          in
+            ''
+              export JAVA_TOOL_OPTIONS="${loadLombok}${prev}"
+            '';
       });
     };
 }
